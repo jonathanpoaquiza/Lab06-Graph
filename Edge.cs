cb9a3e6752a64fc49e19e34c4a8a16d79cd24fb1
Jonathan Poaquiza <jonathan.poaquiza@epn.edu.ec>
2023-06-15 17:23:55 -0500

    First Version

---
+++ Edge.cs
@@ -0,0 +1,12 @@
+namespace Lab06_Graph;
+public class Edge<T>
+{
+    public Node<T> From { get; set; }
+    public Node<T> To { get; set; }
+    public int Weight { get; set; }
+    public override string ToString()
+    {
+        return $"Edge: {From.Data} -> {To.Data}, weight: {Weight}";
+    }
+}
+
---
+++ Node.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab06_Graph;
+public class Node<T>
+{
+    public int Index { get; set; }
+    public T Data { get; set; }
+    public List<Node<T>> Neighbors { get; set; } = new List<Node<T>>();
+    public List<int> Weights { get; set; } = new List<int>();
+    public override string ToString()
+    {
+        return $"Node with index {Index}: {Data},neighbors: {Neighbors.Count}";
+    }
+}
+
+
